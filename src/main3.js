d790ce1fa0ec9e9aea1d2f73c24069f77d464d32
quasar1533 <1029230641@qq.com>
2020-11-18 20:03:07 +0800

    demo-1

--- src/main3.js
+++ src/main3.js
@@ -3,6 +3,25 @@ let span = document.createElement("span");
 span.innerText = "click me";
 let div = document.querySelector("#button");
 div.appendChild(btn);
-let div2 = document.createElement("div");
-div2.style;
 btn.appendChild(span);
+
+function on(eventType, element, selector, callback) {
+  if (!(element instanceof Element)) {
+    element = document.querySelector(element);
+  }
+  element.addEventListener(eventType, (e) => {
+    let t = e.target;
+    while (!t.matches(selector)) {
+      if (t === element) {
+        t = null;
+        break;
+      }
+      t = t.parentNode;
+    }
+    t && callback.call(t, e, t); //针对事件的函数，所以传入事件和选定的节点本身
+  });
+} //传入想要委托的元素与能够触发事件的子元素的选择器
+
+on("click", "#button", "button", (e) => {
+  console.log("hello!");
+});
--- test.js
+++ test.js
@@ -1,4 +1,17 @@
 //实现一个自动添加事件委托的函数
-function on (eventType, element, selector, callback) {
-  
-}
+function on(eventType, element, selector, callback) {
+  if (!(element instanceof Element)) {
+    element = document.querySelector(element);
+  }
+  element.addEventListener(eventType, (e) => {
+    let t = e.target;
+    while (!t.matches(selector)) {
+      if (t === element) {
+        t = null;
+        break;
+      }
+      t = t.parentNode;
+    }
+    t && callback.call(t, e, t); //针对事件的函数，所以传入事件和选定的节点本身
+  });
+} //传入想要委托的元素与能够触发事件的子元素的选择器
